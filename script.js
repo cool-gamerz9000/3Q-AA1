ade802ea1b42838e56d766b7baa9dc2975aa8b5f
cool-gamerz9000 <jpvhacate22@src.pshs.edu.ph>
2025-01-22 11:30:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var pi=3.14159265359;
 var radius=10;
 var circumference=2*pi*radius;
+var diamenter=2*radius;
+var surfacearea=4*pi*radius**2;
+var volume=1.33333333333*pi*radius**3;
 document.write('The radius of the sphere is '+radius);
+document.write('\n');
 document.write('The circumference is'+circumference);
+
+
