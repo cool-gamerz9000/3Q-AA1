9ff5139fcd70fac827244a69a8d417bfec9db15a
cool-gamerz9000 <jpvhacate22@src.pshs.edu.ph>
2025-01-22 12:13:00 +0800

    script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var circumference=2*pi*radius;
 var diameter=2*radius;
 var surfacearea=4*pi*radius**2;
 var volume=1.33333333333*pi*radius**3;
-document.write('The radius of the sphere is '+radius '.<br>');
-document.write('The circumference is '+circumference '.<br>');
-document.write('The surface area is '+surfacearea '.<br>');
-document.write('The volume is '+volume '.<br>');
+document.write('The radius of the sphere is '+radius+ '.<br>');
+document.write('The circumference is '+circumference+ '.<br>');
+document.write('The surface area is '+surfacearea+ '.<br>');
+document.write('The volume is '+volume+ '.<br>');
 
 
